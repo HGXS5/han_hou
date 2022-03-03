fc8b7958a02697098489b7cf1561da2fd942bf5c
han <15638258963@163.com>
2022-03-03 17:40:48 +0800

    eureka服务端工程建立，cms配置成eureka客户端，创建course工程

--- xc-framework-model/src/main/java/com/xuecheng/framework/domain/course/Category.java
+++ xc-framework-model/src/main/java/com/xuecheng/framework/domain/course/Category.java
@@ -14,7 +14,9 @@ import java.io.Serializable;
 @ToString
 @Entity
 @Table(name="category")
+//assigned在插入数据的时候主键由程序处理（很常用），这是 <generator>元素没有指定时的默认生成策略
 @GenericGenerator(name = "jpa-assigned", strategy = "assigned")
+// 采用128位的uuid算法生成主键，uuid被编码为一个32位16进制数字的字符串。占用空间大（字符串类型）。
 //@GenericGenerator(name = "jpa-uuid", strategy = "uuid")
 public class Category implements Serializable {
     private static final long serialVersionUID = -906357110051689484L;
--- xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/ManageCmsApplication.java
+++ xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/ManageCmsApplication.java
@@ -3,6 +3,7 @@ package com.xuecheng.manage_cms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
@@ -14,6 +15,7 @@ import org.springframework.web.client.RestTemplate;
  * @create 2018-09-12 17:13
  **/
 @SpringBootApplication
+@EnableDiscoveryClient
 @EntityScan("com.xuecheng.framework.domain.cms")//扫描实体类
 @ComponentScan(basePackages={"com.xuecheng.api"})//扫描接口
 @ComponentScan(basePackages={"com.xuecheng.manage_cms"})//扫描本项目下的所有类
